aad5775c57e79756274a6b61a13cd1e672568b53
Alejandro Sáez <alejandro.sapa@gmail.com>
2025-11-19 07:00:56 -0300

    chore: update package version to 0.0.2 and modify Callout component styles
    - Updated package.json to version 0.0.2.
    - Changed Callout component background and text to reflect new messaging.
    - Adjusted layout in Callout component for better responsiveness.
    - Commented out the button in Nav component for future use.
    - Modified positioning of Callout in the main page layout.

--- src/components/Callout.svelte
+++ src/components/Callout.svelte
@@ -3,13 +3,13 @@
 </script>
 
 <div
-	class="bg-primary grid mx-auto place-items-center px-4 w-full md:w-[600px] rounded-xl shadow-lg text-accent"
+	class="bg-error/70 backdrop-blur-xl grid mx-auto place-items-center px-4 w-full md:w-[600px] md:rounded-md shadow-lg text-accent"
 >
-	<div class="flex gap-2 px-4 py-8">
+	<div class="flex flex-col md:flex-row items-center gap-2 px-4 py-8">
         <OctagonAlert size={64} />
-		<div>
-			<h2 class="font-bold text-lg uppercase text-pretty ">Los entrenamientos se están realizando con normalidad.</h2>
-            <p class="text-lg mt-2 text-pretty">Última actualización: Jueves 24 de Julio, 2025</p>
+		<div class="text-center md:text-left">
+			<h2 class="font-bold text-lg uppercase text-pretty">Por el momento no hay nuevas inscripciones.</h2>
+            <p class="text-lg text-pretty">Última actualización: 19 de noviembre, 2025</p>
 		</div>
 	</div>
 </div>
