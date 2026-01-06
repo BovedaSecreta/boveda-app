dfb257d4e02365ad2a5bea60d8baa25fb3de27f5
Alejandro Sáez <alejandro.sapa@gmail.com>
2026-01-06 14:55:03 -0300

    feat: timeline

---
+++ src/components/Timeline.svelte
@@ -0,0 +1,62 @@
+<ul class="timeline timeline-vertical lg:timeline-horizontal mx-auto">
+    <li>
+        <hr class="bg-indigo-800" />
+		<div class="timeline-start">Vie. 30 Enero</div>
+		<div class="timeline-middle">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				viewBox="0 0 20 20"
+				fill="currentColor"
+				class="h-5 w-5"
+			>
+				<path
+					fill-rule="evenodd"
+					d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+					clip-rule="evenodd"
+				/>
+			</svg>
+		</div>
+		<div class="timeline-end timeline-box"><b>Examen Básico</b></div>
+		<hr />
+	</li>
+	<li>
+		<hr />
+		<div class="timeline-start">Sáb. 07 Febrero</div>
+		<div class="timeline-middle">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				viewBox="0 0 20 20"
+				fill="currentColor"
+				class="h-5 w-5"
+			>
+				<path
+					fill-rule="evenodd"
+					d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+					clip-rule="evenodd"
+				/>
+			</svg>
+		</div>
+		<div class="timeline-end timeline-box"><b>Examen Intermedio</b></div>
+		<hr />
+	</li>
+	<li>
+		<hr />
+		<div class="timeline-start">Vie. 13 Febrero</div>
+		<div class="timeline-middle">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				viewBox="0 0 20 20"
+				fill="currentColor"
+				class="h-5 w-5"
+			>
+				<path
+					fill-rule="evenodd"
+					d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+					clip-rule="evenodd"
+				/>
+			</svg>
+		</div>
+		<div class="timeline-end timeline-box"><b>Crudo</b></div>
+            <hr />
+	</li>
+</ul>
