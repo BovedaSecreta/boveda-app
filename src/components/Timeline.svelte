b8f47fd98659cba4187e5171cda2f8c79c5257a4
Alejandro Sáez <alejandro.sapa@gmail.com>
2026-01-14 11:16:46 -0300

    feat: adding info to timeline

--- src/components/Timeline.svelte
+++ src/components/Timeline.svelte
@@ -1,6 +1,42 @@
+<script>
+	import { Info } from 'lucide-svelte';
+</script>
+
+<!-- Open the modal using ID.showModal() method -->
+<dialog id="my_modal_1" class="modal">
+	<div class="modal-box">
+		<h3 class="text-lg font-bold">Examen Básico - Vie. 30 Enero</h3>
+		<div class="py-4 space-y-4">
+			<div role="alert" class="alert alert-info">
+				<span>El examen no tiene costo adicional</span>
+			</div>
+			<ul class="mt-4 text-sm">
+				<li><b>Horario</b>: Por definir.</li>
+				<li><b>Lugar</b>: Bóveda Secreta</li>
+				<li><b>Duración aprox</b>: 4-5 horas</li>
+			</ul>
+			<div class="text-sm">
+				Para rendirlo sólo deben contar con un plan activo y haber cursado 6 meses continuos
+				entrenando en la bóveda secreta. <br/><br/>La vestimenta es la misma q se recomienda en los
+				entrenamientos, ropa cómoda, polera con mangas y llevar cambio de polera, se recomienda
+				tambien llevar toalla personal, y calzado apropiado o derechamente no con el que llegan al
+				recinto. <br/><br/> El examen mide las habilidades cognitivas, físicas y comunicación que se han
+				adquirido en los entrenamientos. Se mide si un alumno pasa o no si sus bases son firmes y
+				está capacitado para recibir más información.
+			</div>
+		</div>
+		<div class="modal-action">
+			<form method="dialog">
+				<!-- if there is a button in form, it will close the modal -->
+				<button class="btn">Cerrar</button>
+			</form>
+		</div>
+	</div>
+</dialog>
+
 <ul class="timeline timeline-vertical lg:timeline-horizontal mx-auto">
-    <li>
-        <hr class="bg-indigo-800" />
+	<li>
+		<hr class="bg-indigo-800" />
 		<div class="timeline-start">Vie. 30 Enero</div>
 		<div class="timeline-middle">
 			<svg
@@ -16,7 +52,9 @@
 				/>
 			</svg>
 		</div>
-		<div class="timeline-end timeline-box"><b>Examen Básico</b></div>
+		<button class="timeline-end timeline-box btn" onclick="my_modal_1.showModal()"
+			><b>Examen Básico</b> <Info /></button
+		>
 		<hr />
 	</li>
 	<li>
@@ -57,6 +95,6 @@
 			</svg>
 		</div>
 		<div class="timeline-end timeline-box"><b>Crudo</b></div>
-            <hr />
+		<hr />
 	</li>
 </ul>
